9fb169de8d55bb8c7cc07bb401a25bd41ff614c3
leeprince <leeprince@foxmail.com>
2021-07-23 01:38:20 +0800

    style: 注释

--- router/v3/routers/init.go
+++ router/v3/routers/init.go
@@ -8,12 +8,16 @@ type Router func(*gin.Engine)
 // 自动注册路由的切片
 var routers = []Router{}
 
+// 注册路由
 func RegisterRouter(router ...Router)  {
 	routers = append(routers, router...)
 }
 
+// 初始化路由
 func IniterRouter() *gin.Engine  {
 	r := gin.Default()
+	
+	// 初始化路由
 	for _, router := range routers {
 		router(r)
 	}
